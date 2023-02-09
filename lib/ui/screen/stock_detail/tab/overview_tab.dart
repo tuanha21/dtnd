6bac0dad8bac1ffdeb4a0ec5df1353c6c32d2ac6
ltthinhtb <ltthinhtb@gmail.com>
2023-02-09 16:26:47 +0700

    tổng quan chi tiết mã

--- lib/ui/screen/stock_detail/tab/overview_tab.dart
+++ lib/ui/screen/stock_detail/tab/overview_tab.dart
@@ -2,6 +2,7 @@ import 'package:dtnd/=models=/response/stock_model.dart';
 import 'package:flutter/material.dart';
 
 import '../../../../generated/l10n.dart';
+import '../../../theme/app_color.dart';
 import '../../home/widget/home_section.dart';
 import '../widget/component/price_alert.dart';
 import '../widget/stock_detail_chart.dart';
@@ -27,18 +28,61 @@ class _OverviewTabState extends State<OverviewTab> {
         SizedBox(
             height: 200,
             child: StockDetailChart(stockModel: widget.stockModel)),
-        const Padding(
-          padding: EdgeInsets.symmetric(horizontal: 16),
-          child: PriceAlert(),
+        // const Padding(
+        //   padding: EdgeInsets.symmetric(horizontal: 16),
+        //   child: PriceAlert(),
+        // ),
+        // const SizedBox(height: 24),
+        HomeSection(
+          title: "Chỉ số cơ bản",
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Giá thấp nhất',
+                      style: Theme.of(context)
+                          .textTheme
+                          .labelMedium
+                          ?.copyWith(color: AppColors.neutral_04),
+                    ),
+                    Text(
+                      'Giá cao nhất',
+                      style: Theme.of(context)
+                          .textTheme
+                          .labelMedium
+                          ?.copyWith(color: AppColors.neutral_04),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      widget.stockModel.stockData.lowPrice.value.toString(),
+                      style: Theme.of(context).textTheme.titleSmall,
+                    ),
+                    Text(
+                      widget.stockModel.stockData.highPrice.value.toString(),
+                      style: Theme.of(context).textTheme.titleSmall,
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
         ),
-        const SizedBox(height: 24),
+        const SizedBox(height: 20),
         HomeSection(
           title: S.of(context).news,
           child: Padding(
             padding: const EdgeInsets.symmetric(horizontal: 16),
-            child: StockDetailNews(
-              stockCode: widget.stockModel.stock.stockCode
-            ),
+            child:
+                StockDetailNews(stockCode: widget.stockModel.stock.stockCode),
           ),
         )
       ],
