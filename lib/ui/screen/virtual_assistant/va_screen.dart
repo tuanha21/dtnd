30fe7863c86adda8c1f6551e7071661242791baa
ltthinhtb <ltthinhtb@gmail.com>
2023-02-21 16:33:58 +0700

    lọc cổ phiếu

--- lib/ui/screen/virtual_assistant/va_screen.dart
+++ lib/ui/screen/virtual_assistant/va_screen.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-
 import 'package:dtnd/config/service/app_services.dart';
 import 'package:dtnd/generated/l10n.dart';
 import 'package:dtnd/ui/screen/virtual_assistant/va_filter/virtual_assistant_filter_screen.dart';
@@ -8,7 +7,6 @@ import 'package:dtnd/ui/screen/virtual_assistant/va_volatolity_warning/va_volato
 import 'package:dtnd/ui/theme/app_color.dart';
 import 'package:dtnd/ui/theme/app_image.dart';
 import 'package:dtnd/utilities/logger.dart';
-import 'package:dtnd/utilities/responsive.dart';
 import 'package:flutter/material.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
 import 'package:msgpack_dart/msgpack_dart.dart' as msgp;
@@ -24,7 +22,7 @@ extension VirtualAssistantFeatureX on VAFeature {
       case VAFeature.stockFilter:
         return S.current.filter_stock;
       case VAFeature.volatilityWarning:
-        return S.current.volatility_warning;
+        return "Lọc tín hiệu";
     }
   }
 
@@ -59,27 +57,6 @@ class VAScreen extends StatefulWidget {
 }
 
 class _VAScreenState extends State<VAScreen> {
-  bool initialized = false;
-  WebSocketChannel? channel;
-  Timer? timer;
-
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  Future<void> init() async {
-    final registered = await Navigator.of(context).push(MaterialPageRoute(
-      builder: (context) => const VARegister(),
-    ));
-    if (registered) {
-      print("setState");
-      setState(() {
-        initialized = true;
-      });
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     final themeMode = AppService.instance.themeMode.value;
@@ -115,189 +92,25 @@ class _VAScreenState extends State<VAScreen> {
           style: textTheme.labelLarge?.copyWith(fontWeight: FontWeight.w700),
         ),
       ),
-      body: Center(
-        child: Column(
-          children: [
-            SizedBox(
-              height: 52,
-              child: ListView.separated(
-                padding: const EdgeInsets.symmetric(horizontal: 16),
-                itemCount: VAFeature.values.length,
-                scrollDirection: Axis.horizontal,
-                // shrinkWrap: true,
-                itemBuilder: (context, index) {
-                  Widget child = Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      SizedBox.square(
-                        dimension: 24,
-                        child: Image.asset(VAFeature.values[index].iconPath),
-                      ),
-                      Text(
-                        VAFeature.values[index].name,
-                      )
-                    ],
-                  );
-                  child = InkWell(
-                    onTap: VAFeature.values[index].onPressed(context),
-                    child: Ink(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 8, vertical: 4),
-                      // width: 102,
-                      // height: 52,
-                      decoration: const BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.all(Radius.circular(8))),
-                      child: child,
-                    ),
-                  );
-
-                  // TextButton(
-                  //     onPressed: VirtualAssistantFeature.values[index]
-                  //         .onPressed(context),
-                  //     child: Text(VirtualAssistantFeature.values[index].name));
-                  return child;
-                },
-                separatorBuilder: (BuildContext context, int index) =>
-                    const SizedBox(
-                  width: 8,
-                ),
-              ),
+      body: ListView.separated(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        itemCount: VAFeature.values.length,
+        // shrinkWrap: true,
+        itemBuilder: (context, index) {
+          return ListTile(
+            onTap: VAFeature.values[index].onPressed(context),
+            leading: SizedBox.square(
+              dimension: 24,
+              child: Image.asset(VAFeature.values[index].iconPath),
             ),
-            Text(S.of(context).virtual_assistant),
-            SizedBox(
-              width: Responsive.getMaxWidth(context) - 32,
-              child: TextButton(
-                onPressed: connectWs,
-                style: const ButtonStyle(
-                    padding: MaterialStatePropertyAll(EdgeInsets.all(14))),
-                child: const Text("Connect WS"),
-              ),
-            ),
-            const SizedBox(height: 10),
-            SizedBox(
-              width: Responsive.getMaxWidth(context) - 32,
-              child: TextButton(
-                onPressed: disconnectWs,
-                style: const ButtonStyle(
-                    padding: MaterialStatePropertyAll(EdgeInsets.all(14))),
-                child: const Text("Disconnect WS"),
-              ),
-            ),
-            const SizedBox(height: 10),
-            SizedBox(
-              width: Responsive.getMaxWidth(context) - 32,
-              child: TextButton(
-                onPressed: sendMsg,
-                style: const ButtonStyle(
-                    padding: MaterialStatePropertyAll(EdgeInsets.all(14))),
-                child: const Text("Send msg"),
-              ),
-            ),
-          ],
+            title: Text(VAFeature.values[index].name),
+            trailing: const Icon(Icons.chevron_right_outlined),
+          );
+        },
+        separatorBuilder: (BuildContext context, int index) => const SizedBox(
+          width: 8,
         ),
       ),
     );
-  }
-
-  void sendMsg() {
-    channel?.sink.add(msgp.serialize({"msg": "ping"}));
-  }
-
-  void connectWs() async {
-    try {
-      if (channel != null) {
-        SnackBar snackBar = const SnackBar(
-          content: Text("Already connected"),
-        );
-        ScaffoldMessenger.of(context).removeCurrentSnackBar();
-        ScaffoldMessenger.of(context).showSnackBar(snackBar);
-        return;
-      }
-      channel = WebSocketChannel.connect(
-        Uri.parse('ws://192.168.100.58:8888'),
-      );
-      timer = Timer.periodic(const Duration(seconds: 1), (timer) {
-        print("ping");
-        // channel!.sink.add("ping");
-        channel!.sink.add(msgp.serialize({"msg": "ping"}));
-        // channel!.sink.addError("addError");
-      });
-      SnackBar snackBar = const SnackBar(
-        content: Text("Connected"),
-      );
-      ScaffoldMessenger.of(context).removeCurrentSnackBar();
-      ScaffoldMessenger.of(context).showSnackBar(snackBar);
-      channel?.stream.listen(
-        (event) {
-          print(event);
-          // if (event is String) {
-          //   return;
-          // }
-
-          // final u = Unpacker.fromList(event);
-          final msg = msgp.deserialize(event);
-          print(msg);
-          // final msg1 = msgp.deserialize(msg);
-          // print(u);
-          // print(msg1);
-          // final i = u.unpackMapLength();
-          // print(i);
-          // final c = u.unpackMap();
-          // print(c);
-          // if (u.runtimeType) {
-
-          // }
-          // final r = jsonDecode(u);
-          // print(r['Key_0']);
-          // final id = u.unpackString();
-          // logger.v(id);
-          // final userId = u.unpackString();
-          // print(userId);
-          // logger.v(userId);
-        },
-        onError: (error) => logger.e("onError"),
-        onDone: () {
-          disconnectWs();
-          print("disconnected");
-        },
-      );
-    } catch (e) {
-      SnackBar snackBar = SnackBar(
-        content: Text(e.toString()),
-      );
-
-      ScaffoldMessenger.of(context).removeCurrentSnackBar();
-      ScaffoldMessenger.of(context).showSnackBar(snackBar);
-    }
-  }
-
-  void disconnectWs() async {
-    try {
-      if (channel == null) {
-        SnackBar snackBar = const SnackBar(
-          content: Text("Already disconnected"),
-        );
-        ScaffoldMessenger.of(context).removeCurrentSnackBar();
-        ScaffoldMessenger.of(context).showSnackBar(snackBar);
-        return;
-      }
-      timer?.cancel();
-      channel!.sink.close();
-      channel = null;
-
-      SnackBar snackBar = const SnackBar(
-        content: Text("Disconnected"),
-      );
-      ScaffoldMessenger.of(context).removeCurrentSnackBar();
-      ScaffoldMessenger.of(context).showSnackBar(snackBar);
-    } catch (e) {
-      SnackBar snackBar = SnackBar(
-        content: Text(e.toString()),
-      );
-
-      ScaffoldMessenger.of(context).removeCurrentSnackBar();
-      ScaffoldMessenger.of(context).showSnackBar(snackBar);
-    }
   }
 }
