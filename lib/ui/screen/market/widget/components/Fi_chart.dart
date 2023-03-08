6ebd267d1712cdd8dfd8483318f286156f1d9d91
khanhnx1312 <khanhnx1312@gmail.com>
2023-03-08 16:45:44 +0700

    fixbug

--- lib/ui/screen/market/widget/components/Fi_chart.dart
+++ lib/ui/screen/market/widget/components/Fi_chart.dart
@@ -72,7 +72,7 @@ class _FiChartValueState extends State<FiChartValue> {
                                 decoration:
                                     const BoxDecoration(color: Colors.white),
                                 child: Text(
-                                  '${tile.group} : ${NumUtils.formatInteger(tile.weight)}',
+                                  '${tile.group} : ${NumUtils.formatInteger(tile.weight)} (triệu)',
                                   overflow: TextOverflow.ellipsis,
                                   textAlign: TextAlign.center,
                                 ),
