6e690ff4ec268c0f291107e685c03f6ab951ae66
Duc Phuc <the20thcenturycat@gmail.com>
2023-04-11 17:19:29 +0700

    init

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -40,7 +40,7 @@ class DefaultFirebaseOptions {
       case TargetPlatform.linux:
         throw UnsupportedError(
           'DefaultFirebaseOptions have not been configured for linux - '
-          'you can reconfigure this by running the FlutterFire CLI again..',
+          'you can reconfigure this by running the FlutterFire CLI again.',
         );
       default:
         throw UnsupportedError(
