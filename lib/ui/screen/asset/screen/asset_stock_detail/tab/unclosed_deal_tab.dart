50887b1dda2cffa4e4669746359c2e529a4ab7fd
tiennh <huutienact@gmail.com>
2023-04-28 15:16:43 +0700

    update code 28-4

--- lib/ui/screen/asset/screen/asset_stock_detail/tab/unclosed_deal_tab.dart
+++ lib/ui/screen/asset/screen/asset_stock_detail/tab/unclosed_deal_tab.dart
@@ -1,4 +1,5 @@
 import 'package:dtnd/=models=/response/order_history_model.dart';
+import 'package:dtnd/=models=/side.dart';
 import 'package:dtnd/data/i_exchange_service.dart';
 import 'package:dtnd/data/i_user_service.dart';
 import 'package:dtnd/data/implementations/exchange_service.dart';
@@ -6,13 +7,14 @@ import 'package:dtnd/data/implementations/user_service.dart';
 import 'package:dtnd/ui/screen/asset/screen/asset_stock_detail/component/unclosed_deal_element.dart';
 import 'package:dtnd/ui/theme/app_color.dart';
 import 'package:dtnd/ui/widget/empty_list_widget.dart';
+import 'package:dtnd/utilities/num_utils.dart';
 import 'package:flutter/material.dart';
-
-import '../../../../../../utilities/num_utils.dart';
 
 class UnclosedDealTab extends StatefulWidget {
   const UnclosedDealTab({super.key, required this.stockCode});
+
   final String stockCode;
+
   @override
   State<UnclosedDealTab> createState() => _UnclosedDealTabState();
 }
@@ -36,7 +38,11 @@ class _UnclosedDealTabState extends State<UnclosedDealTab>
         stockCode: widget.stockCode, status: "M");
     // logger.v(res);
     if (res.isNotEmpty) {
-      list.addAll(res);
+      for (OrderHistoryModel data in res) {
+        if (data.side == Side.buy) {
+          list.add(data);
+        }
+      }
       setState(() {});
     }
   }
@@ -45,6 +51,16 @@ class _UnclosedDealTabState extends State<UnclosedDealTab>
   Widget build(BuildContext context) {
     super.build(context);
     final textTheme = Theme.of(context).textTheme;
+    final portfolioStocks =
+        userService.defaultAccount.value?.portfolioStatus?.porfolioStocks;
+
+    final gainLossValue = portfolioStocks
+        ?.firstWhere((element) => element.symbol == widget.stockCode)
+        .gainLossValue;
+
+    final actualVol = portfolioStocks
+        ?.firstWhere((element) => element.symbol == widget.stockCode)
+        .actualVol;
 
     return SingleChildScrollView(
       child: Column(
@@ -85,7 +101,6 @@ class _UnclosedDealTabState extends State<UnclosedDealTab>
                 //     ),
                 //   ],
                 // ),
-                const SizedBox(height: 8),
                 Row(
                   crossAxisAlignment: CrossAxisAlignment.center,
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -95,8 +110,8 @@ class _UnclosedDealTabState extends State<UnclosedDealTab>
                       style: textTheme.labelMedium
                           ?.copyWith(fontWeight: FontWeight.w600),
                     ),
-                    Text('${NumUtils.formatDouble(userService.defaultAccount.value?.portfolioStatus
-                        ?.gainLossValue)}đ',
+                    Text(
+                      '${NumUtils.formatDouble(gainLossValue)}đ',
                       style: textTheme.labelMedium,
                     ),
                   ],
@@ -105,31 +120,33 @@ class _UnclosedDealTabState extends State<UnclosedDealTab>
             ),
           ),
           const SizedBox(height: 6),
-          Container(
-            padding: const EdgeInsets.all(16),
-            decoration: const BoxDecoration(
-                color: AppColors.neutral_06,
-                borderRadius: BorderRadius.all(Radius.circular(12))),
-            child: Column(
-              children: [
-                if (list.isEmpty)
-                  const Padding(
-                    padding: EdgeInsets.symmetric(vertical: 16),
-                    child: EmptyListWidget(),
-                  ),
-                for (int i = 0; i < list.length; i++)
-                  Column(
+          actualVol != 0
+              ? Container(
+                  padding: const EdgeInsets.all(16),
+                  decoration: const BoxDecoration(
+                      color: AppColors.neutral_06,
+                      borderRadius: BorderRadius.all(Radius.circular(12))),
+                  child: Column(
                     children: [
-                      if (i != 0)
-                        const Divider(
-                          height: 17,
+                      if (list.isEmpty)
+                        const Padding(
+                          padding: EdgeInsets.symmetric(vertical: 16),
+                          child: EmptyListWidget(),
                         ),
-                      UnclosedDealElement(model: list.elementAt(i)),
+                      for (int i = 0; i < list.length; i++)
+                        Column(
+                          children: [
+                            if (i != 0)
+                              const Divider(
+                                height: 17,
+                              ),
+                            UnclosedDealElement(model: list.elementAt(i)),
+                          ],
+                        )
                     ],
-                  )
-              ],
-            ),
-          ),
+                  ),
+                )
+              : const EmptyListWidget(),
           const SizedBox(height: 80),
         ],
       ),
